4cfa7defc1b25fc370459a56dc854517a6a6d84c
AceMan <macebedo619@gmail.com>
2015-12-01 06:08:20 -0800

    Update variable.inc.php

--- include/variable.inc.php
+++ include/variable.inc.php
@@ -4,7 +4,7 @@
   $host = "sql107.byethost10.com";
   $dbname = "b10_16807313_acebedo";
   $dbuser = "b10_16807313";
-  $pwd = "Dublin2013";
+  $pwd = "";
   $dbc =0;
   $dbc = mysqli_connect($host, $dbuser, $pwd, $dbname)
         or die ('Cannot connect to database');
